bc1837ef3b6aca31eb0fa2df48c6a2b8682af3fd
jitendra <jitendra_mahato@gmail.com>
2022-03-30 22:02:04 +0530

    add

--- Day_4_Loop/while_3.py
+++ Day_4_Loop/while_3.py
@@ -1,5 +1,9 @@
 n=int(input())
 i=1
+if i%2==0:
+    n=n-1
+else:
+    n=n
 while i<=n:
     print(i,end=" ")
-    i=i+1
+    i=i+2
---
+++ Day_4_Loop/while_4.py
@@ -0,0 +1,5 @@
+n=int(input())
+while i<=n:
+    if i%4==0:
+        print(i)
+    i=i+4
